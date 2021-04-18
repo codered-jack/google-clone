8983df3bb22cbca814c277d3e2c55ef5d8861e2a
Sumit Kumar <sumit.kumar@pinelabs.com>
2021-04-18 14:18:20 +0530

    google-2.0

---
+++ keys.js
@@ -0,0 +1,5 @@
+//API Key -https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = 'AIzaSyAZsSua6_neD_Zs8KDq-qhstffUKzEHz1A';
+//Context Key - https://cse.google.com/cse/create/new
+
+export const CONTEXT_KEY ='b610bceb9cfccb982';
